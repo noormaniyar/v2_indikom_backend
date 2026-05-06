0278359ae87d7ca360bd1cc5bbc30ca8440c07ad
Noor at Mac <nooruddin2727@gmail.com>
2026-05-07 02:37:38 +0530

    migrations for delivery, notifications, payments, subscriptions

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # path('register/', views.RegisterView.as_view(), name='register'),
     # path('login/', views.CustomTokenObtainPairView.as_view(), name='login'),
     # path('logout/', views.LogoutView.as_view(), name='logout'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 
     # Profile
     path('me/', views.MeView.as_view(), name='me'),
